2989aaa31e9b2072d1d6b636d0af8bfed1716142
LeandroFLourenco <leandrinlourenco@gmail.com>
2026-08-14 18:11:58 -0300

    feat:Realizado estudo de switch e laco

--- Assets/variaveis.cs
+++ Assets/variaveis.cs
@@ -3,20 +3,57 @@ using UnityEngine;
 
 public class variaveis : MonoBehaviour
 {
-    int moedas;
+    int estados = 1;
+    
 
     void Start()
     {
-        moedas = 10;        
+        //if (estados == 1)
+        //{
+        //    print("Mate o heroi!");
+        //}
+        //else if (estados == 2)
+        //{
+        //    print("Mate se puder!");
+        //}
+        //else if (estados == 3)
+        //{
+        //    print("Corra!");
+        //}
+        //else
+        //{
+        //    print("Nenhum estado selecionado!");
+        //}
+
+        //    switch (estados)
+        //    {
+        //        case 1:
+        //            print("Mate o heroi!");
+        //            break;
+        //        case 2:
+        //            print("Mate se puder!");
+        //            break;
+        //        case 3:
+        //            print("Corra!");
+        //            break;
+        //        default:
+        //            print("Nenhum estado selecionado!");
+        //            break;
+        //    }
+
+        for (int x = 0 ; (x > 0 && x <= 10); x++)
+        {
+            print("Hello World!" + " " + "-" + " " + x);
+        }
     }
 
     void Update()
     {
-        moedas += 1;
+        //moedas += 1;
 
-        if(moedas == 15)
-        {
-            print(moedas);
-        }
+        //if(moedas == 15)
+        //{
+        //    print(moedas);
+        //}
     }
 }
